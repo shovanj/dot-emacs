b7645c819ebf6abe36913eefcb64eac293ad582f
Shovan Joshi <shovanj@gmail.com>
2012-08-20 16:18:10 -0600

    add coffee mode config

--- init.el
+++ init.el
@@ -269,3 +269,34 @@
   (setq c-basic-offset 4)
   (setq mumamo-background-colors nil))
 (add-hook 'php-mode-hook 'wicked/php-mode-init)
+
+
+(add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
+(add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
+
+(setq whitespace-action '(auto-cleanup)) ;; automatically clean up bad whitespace
+(setq whitespace-style '(trailing space-before-tab indentation empty space-after-tab)) ;; only show bad whitespace
+
+(defun coffee-custom ()
+  "coffee-mode-hook"
+
+  ;; CoffeeScript uses two spaces.
+  (make-local-variable 'tab-width)
+  (set 'tab-width 2)
+
+  ;; If you don't want your compiled files to be wrapped
+  ;; (setq coffee-args-compile '("-c" "--bare"))
+
+  ;; Emacs key binding
+  (define-key coffee-mode-map [(meta r)] 'coffee-compile-buffer)
+
+  ;; Riding edge.
+  ;; (setq coffee-command "~/dev/coffee")
+
+  ;; Compile '.coffee' files on every save
+  ;; (and (file-exists-p (buffer-file-name))
+  ;;      (file-exists-p (coffee-compiled-file-name))
+  ;;      (coffee-cos-mode t))
+  )
+
+(add-hook 'coffee-mode-hook 'coffee-custom)
