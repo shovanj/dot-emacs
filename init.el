741aff9b7c12fa454017eef533abeeeb15a198bb
Shovan Joshi <shovanj@gmail.com>
2012-10-31 13:55:11 -0600

    remove modeline status modifcations, use solarize-theme only for GUI version

--- init.el
+++ init.el
@@ -108,9 +108,7 @@
  '(speedbar-show-unknown-files t)
  '(speedbar-use-images nil t)
  '(sr-speedbar-right-side nil)
- '(sr-speedbar-skip-other-window-p t)
- '(tool-bar-mode nil))
-
+ '(sr-speedbar-skip-other-window-p t))
 
 ;; ruby mode
 (add-hook 'ruby-mode-hook (lambda () (local-set-key "\r" 'newline-and-indent)))
@@ -376,79 +374,10 @@
 
 (require 'autopair)
 
-
-
-
-;; use setq-default to set it for /all/ modes
-(setq-default mode-line-format
-  (list
-
-    ;; the buffer name; the file name as a tool tip
-    '(:eval (propertize "%b " 'face 'font-lock-keyword-face
-        'help-echo (buffer-file-name)))
-
-    '(vc-mode vc-mode)
-
-    ;; line and column
-    "(" ;; '%02' to set to 2 chars at least; prevents flickering
-      (propertize "%02l" 'face 'font-lock-type-face) ","
-      (propertize "%02c" 'face 'font-lock-type-face) 
-    ") "
-
-    ;; relative position, size of file
-    "["
-    (propertize "%p" 'face 'font-lock-constant-face) ;; % above top
-    "/"
-    (propertize "%I" 'face 'font-lock-constant-face) ;; size
-    "] "
-
-    ;; the current major mode for the buffer.
-    "["
-
-    '(:eval (propertize "%m" 'face 'font-lock-string-face
-              'help-echo buffer-file-coding-system))
-    "] "
-
-
-    "[" ;; insert vs overwrite mode, input-method in a tooltip
-    '(:eval (propertize (if overwrite-mode "Ovr" "Ins")
-              'face 'font-lock-preprocessor-face
-              'help-echo (concat "Buffer is in "
-                           (if overwrite-mode "overwrite" "insert") " mode")))
-
-
-    ;; is this buffer read-only?
-    '(:eval (when buffer-read-only
-              (concat ","  (propertize "RO"
-                             'face 'font-lock-type-face
-                             'help-echo "Buffer is read-only"))))  
-    "] "
-
-    ;; add the time, with the date and the emacs uptime in the tooltip
-    '(:eval (propertize (format-time-string "%H:%M")
-              'help-echo
-              (concat (format-time-string "%c; ")
-                      (emacs-uptime "Uptime:%hh"))))
-    " --"
-    ;; i don't want to see minor-modes; but if you want, uncomment this:
-    minor-mode-alist  ;; list of minor modes
-
-    ;; was this buffer modified since the last save?
-    '(:eval (when (buffer-modified-p)
-              (concat " "  (propertize "Mod "
-                             'face 'font-lock-warning-face
-                             'help-echo "Buffer has been modified"))))
-
-
-    "%-" ;; fill with '-'
-    ))
-
-
-
-
 (require 'yaml-mode)
 
-
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/emacs-color-theme-solarized")
-(load-theme 'solarized-light t)
-;; (load-theme 'tango-dark t)
+(when window-system
+  (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/emacs-color-theme-solarized")
+  (load-theme 'solarized-light t)
+  ;; (load-theme 'tango-dark t)
+  )
