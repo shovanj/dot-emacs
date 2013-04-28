1af15ff2012553c0e3de89eb981034caff6d0658
Shovan Joshi <shovanj@gmail.com>
2013-04-28 17:51:16 -0600

    organize key bindings and auto detect in separate files. remove rinari, update haml, flymake, ack-and-a-half plugins

--- init.el
+++ init.el
@@ -1,8 +1,8 @@
 (setq backup-directory-alist `(("." . "~/.emacs-backups")))
 (setq backup-by-copying t)
 (setq delete-old-versions t
-      kept-new-versions 6
-      kept-old-versions 2
+      kept-new-versions 20
+      kept-old-versions 20
       version-control t)
 
 ;; ido-mode
@@ -43,8 +43,6 @@
 ;; (setq-default show-trailing-whitespace t)
 (setq-default show-trailing-whitespace nil)
 
-;; disable C-z
-(global-unset-key "\C-z")
 
 ;; use spaces instead of tabs
 (setq-default indent-tabs-mode nil)
@@ -65,7 +63,6 @@
  '(erb-face ((t nil)))
  '(erb-out-delim-face ((t (:background "LightPink1" :foreground "black"))))
  '(erb-out-face ((t nil)))
- '(flycheck-error-face ((t (:inherit error :background "red" :foreground "white"))))
  '(highlight-80+ ((t (:background "gray84"))) t)
  '(magit-diff-add ((t (:foreground "green"))) t)
  '(magit-diff-del ((t (:foreground "violet red"))) t)
@@ -99,39 +96,31 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ack-and-a-half-prompt-for-directory t)
+ '(auto-save-default t)
+ '(auto-save-interval 20)
+ '(auto-save-timeout 10)
  '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default)))
  '(highlight-80+-columns 100)
  '(minimap-width-fraction 0.1)
  '(minimap-window-location (quote right))
  '(org-babel-load-languages (quote ((emacs-lisp . t) (ruby . t))))
  '(org-src-fontify-natively t)
+ '(projectile-show-paths-function (quote projectile-hashify-with-relative-paths))
  '(show-paren-mode t)
  '(speedbar-default-position (quote left))
  '(speedbar-show-unknown-files t)
- '(speedbar-use-images nil t)
+ '(speedbar-use-images t)
  '(sr-speedbar-right-side nil)
  '(sr-speedbar-skip-other-window-p t))
 
 ;; ruby mode
 (add-hook 'ruby-mode-hook (lambda () (local-set-key "\r" 'newline-and-indent)))
-(add-to-list 'auto-mode-alist '("\\.rake\\'" . ruby-mode))
+
 ;; rhtml stuff
 (add-to-list 'load-path "~/.emacs.d/vendor/rhtml")
 (require 'rhtml-mode)
-(add-hook 'rhtml-mode-hook
-          (lambda () (rinari-launch)))
-(add-to-list 'auto-mode-alist '("\\.rhtml\\'" . rhtml-mode))
-(add-to-list 'auto-mode-alist '("\\.html\\.erb\\'" . rhtml-mode))
-(add-to-list 'auto-mode-alist '("\\.erb\\'" . rhtml-mode))
-(add-to-list 'auto-mode-alist '("\\.rfpdf\\'" . rhtml-mode))
-(add-to-list 'auto-mode-alist '("\\.hbs\\'" . rhtml-mode))
-(add-to-list 'auto-mode-alist '("\\.phtml\\'" . php-mode))
-(add-to-list 'auto-mode-alist '("\\.php\\'" . php-mode))
-(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
-(add-to-list 'auto-mode-alist '("\\.eco\\'" . rhtml-mode))
+
+
 
 
 ;; outline minor mode
@@ -143,54 +132,7 @@
              (setq outline-regexp " *\\(def \\|class\\|module\\)")))
 
 
-;; =====================================================================;;
-;; ====================== ibuffer setup ================================;;
 
-(autoload 'ibuffer "ibuffer" "List buffers." t)
-
-(setq ibuffer-saved-filter-groups
-      (quote (("default"
-               ;; Programming Folders
-               ("Terminals" (mode . term-mode))
-               ("Controllers"   (filename . "\\/app\\/controllers"))
-               ("Models"   (filename . "\\/app\\/models"))
-               ("Views"   (filename . "\\/app\\/views"))
-               ("Rspec" (filename . "\\/spec\\/"))
-               ("Helpers"   (filename . "\\/app\\/helpers"))
-               ("Migrations"   (filename . "\\/db\\/"))
-               ("Configurations"   (filename . "\\/config\\/"))
-               ("Lib"   (filename . "\\/lib\\/"))
-               ("View"   (name . "\\.rhtml"))
-               ("View"   (name . "\\.erb"))
-               ("Yaml"   (mode . yaml-mode))
-               ("Cucumber"   (mode . feature-mode))
-               ("Coffee"   (mode . coffee-mode))
-               ("Haml"   (mode . haml-mode))
-               ("Javascript"   (mode . javascript-mode))
-               ("Development"
-                (filename . "\\*Development\\*$"))
-               ("Ruby"   (mode . ruby-mode))
-               ;; Org Mode, Calendar
-               ("Org" (or
-                       (name . "^\\*Calendar\\*$")
-                       (name . "^diary$")
-                       (mode . org-mode)))
-               ("HTML"   (name . "\\.html"))
-               ("Emacs" (or
-                         (name . "^\\*scratch\\*$")
-                         (name . "^\\*GNU Emacs\\*$")
-                         (name . "^\\.")
-                         (name . "\\.el")
-                         (name . "^\\*Messages\\*$")))
-               ("Dired" (mode . dired-mode))))))
-(add-hook 'ibuffer-mode-hook
-          (lambda ()
-            (ibuffer-switch-to-saved-filter-groups "default")))
-
-(setq ibuffer-show-empty-filter-groups nil)
-(setq ibuffer-expert t)
-;; ====================== ibuffer setup ================================;;
-;; =====================================================================;;
 
 (add-hook 'after-init-hook
           '(lambda ()
@@ -254,8 +196,7 @@
             ;; Activate the folding mode
             (hs-minor-mode t)))
 
-(add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
-(add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
+
 
 (setq whitespace-action '(auto-cleanup)) ;; automatically clean up bad whitespace
 (setq whitespace-style '(trailing space-before-tab indentation empty space-after-tab)) ;; only show bad whitespace
@@ -329,15 +270,17 @@
 
 
 
-;; (add-hook 'ruby-mode-hook 'flycheck-mode)
+(require 'flymake-ruby)
+(add-hook 'ruby-mode-hook 'flymake-ruby-load)
+
+
 
 (autopair-global-mode t)
 
 (add-to-list 'load-path "~/.emacs.d/vendor/emacs-nav-49")
 (require 'nav)
 (nav-disable-overeager-window-splitting)
-;; Optional: set up a quick key to toggle nav
-(global-set-key [f8] 'nav-toggle)
+
 
 
 
@@ -350,7 +293,7 @@
  (require 'ob-tangle) 
 
 
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+
 
 (setq ring-bell-function 'ignore)
 
@@ -382,67 +325,6 @@
         (comment-or-uncomment-region beg end)))
 
 
-(when window-system
-  (global-set-key (kbd "s-g") 'magit-status)
-  (global-set-key (kbd "s-b") 'ibuffer)
-  (global-set-key (kbd "<s-left>") 'previous-buffer)
-  (global-set-key (kbd "<s-right>") 'next-buffer)
-  (global-set-key (kbd "s-t") 'projectile-find-file)
-  (global-set-key (kbd "s-/") 'comment-or-uncomment-region-or-line)
-
-
-  ;;(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/emacs-color-theme-solarized")
-  ;;(load-theme 'solarized-light t)
-  )
-
-
-;; etags-select
-(global-set-key (kbd "M-.") 'etags-select-find-tag-at-point)
-(global-set-key (kbd "M-?") 'etags-select-find-tag)
-
-;; window keys
-(global-set-key (kbd "M-o")  'other-window)
-(global-set-key (kbd "M--")  'split-window-vertically)
-(global-set-key (kbd "M-\\") 'split-window-horizontally)
-(global-set-key (kbd "M-0")  'delete-window)
-
-;; window movement keys
-(global-set-key (kbd "C-c <up>")    'windmove-up)    ; Ctl + up arrow
-(global-set-key (kbd "C-c <down>")  'windmove-down)  ; Ctl + down arrow
-(global-set-key (kbd "C-c <right>") 'windmove-right) ; Ctl + right arrow
-(global-set-key (kbd "C-c <left>")  'windmove-left)  ; Ctl + left arrow
-
-
-;; outline minor mode keys
-(global-set-key (kbd "C-c j i") 'hide-body)
-(global-set-key (kbd "C-c j m") 'show-all)
-(global-set-key (kbd "C-c j k") 'show-entry)
-(global-set-key (kbd "C-c j j") 'hide-entry)
-
-;; ibuffer
-(global-set-key (kbd "C-x C-b") 'ibuffer)
-
-;; Invoke M-x without the Alt key
-(global-set-key "\C-x\C-m" 'execute-extended-command)
-(global-set-key "\C-c\C-m" 'execute-extended-command)
-
-; [Ctrl]-[L]
-(global-set-key "\C-l" 'goto-line)
-
-;; expand-region
-(global-set-key (kbd "C-=") 'er/expand-region)
-
-;; Show-hide
-(global-set-key (kbd "C-c <C-right>") 'hs-show-block)
-(global-set-key (kbd "C-c <C-down>") 'hs-show-all)
-(global-set-key (kbd "C-c <C-left>") 'hs-hide-block)
-(global-set-key (kbd "C-c <C-up>") 'hs-hide-all)
-
-(global-set-key (kbd "C-c d") 'desktop-change-dir)
-(global-set-key (kbd "C-c t") 'ansi-term)
-(global-set-key (kbd "C-c e") 'eval-region)
-(global-set-key (kbd "C-c g") 'magit-status)
-(global-set-key (kbd "C-c l") 'magit-log)
 
 
 (when (and (eq system-type 'darwin) window-system)
@@ -468,14 +350,6 @@
 ;; to change fonts 
 ;; M-x list-faces-display
 
-;; (add-hook 'after-init-hook 'global-flycheck-mode)
-;; (add-hook 'ruby-mode-hook 'flycheck-mode)
-
-
-;; (add-hook 'after-init-hook #'global-flycheck-mode)
-
-(add-hook 'after-save-hook 'flycheck-buffer)
-
-;; (add-hook 'ruby-mode-hook
-;;           (lambda () 
-;;              (add-hook 'after-save-hook 'flycheck-buffer)))
+(load "~/.emacs.d/ibuffer")
+(load "~/.emacs.d/detect_mode")
+(load "~/.emacs.d/key_bindings")
