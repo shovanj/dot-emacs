4ddfc3fe4d32e3dca5718a4662afe507f5e1584b
Shovan Joshi <shovanj@gmail.com>
2013-04-30 06:49:44 -0600

    add delete buffer method

--- key_bindings.el
+++ key_bindings.el
@@ -88,3 +88,19 @@
     (call-process "open" nil nil nil "-a" "Finder.app" "."))
   ;; (global-set-key (kbd "s-o") 'osx-finder-open)
   (global-set-key (kbd "C-c o o")    'osx-finder-open))
+
+
+(defun delete-this-buffer-and-file ()
+  "Removes file connected to current buffer and kills buffer."
+  (interactive)
+  (let ((filename (buffer-file-name))
+        (buffer (current-buffer))
+        (name (buffer-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (error "Buffer '%s' is not visiting a file!" name)
+      (when (yes-or-no-p "Are you sure you want to remove this file? ")
+        (delete-file filename)
+        (kill-buffer buffer)
+        (message "File '%s' successfully removed" filename)))))
+
+(global-set-key (kbd "C-c k") 'delete-this-buffer-and-file)
