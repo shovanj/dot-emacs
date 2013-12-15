e21e807f32b8797325ef6b9fb8ceda68c30c6113
Shovan Joshi <shovanj@gmail.com>
2013-12-15 12:39:06 -0700

    add melpa

--- init.el
+++ init.el
@@ -75,6 +75,8 @@
  (desktop-save-mode 1)
 
 (require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (package-initialize)
 (add-to-list 'load-path "~/.emacs.d")    ; This may not be appeared if you have already added.
 (require 'auto-complete-config)
