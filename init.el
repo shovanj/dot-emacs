4219d98ca61939dfc0545cf444d4e1f683ee7a7f
Shovan Joshi <shovanj@gmail.com>
2015-01-29 21:42:16 -0600

    add web-mode

--- init.el
+++ init.el
@@ -127,3 +127,6 @@
 (add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+(add-to-list 'load-path "~/.emacs.d/packages/web-mode/")
+(require 'web-mode)
