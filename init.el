4df181b2110337cfe19f8506cb2235b59813a514
Shovan Joshi <shovanj@gmail.com>
2015-03-26 20:55:49 -0500

    ido vertical

--- init.el
+++ init.el
@@ -184,6 +184,17 @@
 
 
 ;; Display ido results vertically, rather than horizontally
+(setq ido-decorations (quote ("\n-> " "" "\n   " "\n   ..." "[" "]" " [No match]" " [Matched]" " [Not readable]" " [Too big]" " [Confirm]")))
+
+(defun ido-disable-line-truncation () (set (make-local-variable 'truncate-lines) nil))
+(add-hook 'ido-minibuffer-setup-hook 'ido-disable-line-truncation)
+(defun ido-define-keys () ;; C-n/p is more intuitive in vertical layout
+  (define-key ido-completion-map (kbd "C-n") 'ido-next-match)
+  (define-key ido-completion-map (kbd "C-p") 'ido-prev-match))
+(add-hook 'ido-setup-hook 'ido-define-keys)
+
+(global-set-key (kbd "s-o") 'ido-find-file)
+
 (defun tabbar-buffer-groups-by-dir ()
         "Put all files in the same directory into the same tab bar"
         (with-current-buffer (current-buffer)
